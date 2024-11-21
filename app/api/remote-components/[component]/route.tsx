a31c0e86705faa1ddf3a0c6f7a747955fe820d6c
Jonathan Hefner <jonathan@hefner.pro>
2024-11-21 15:05:15 -0600

    Support fowardRef-like behavior

--- app/api/remote-components/[component]/route.tsx
+++ app/api/remote-components/[component]/route.tsx
@@ -1,4 +1,4 @@
-import MyServerComponent from "@/components/MyServerComponent"
+import { MyServerComponent } from "@/components/MyServerComponent"
 import { NextRequest } from "next/server"
 import { PassThrough } from "stream"
 
--- components/MyServerComponent.tsx
+++ components/MyServerComponent.tsx
@@ -1,9 +1,17 @@
-export default async function MyServerComponent({
-  hello,
-  children,
-}: {
-  hello: string,
-  children: React.ReactNode,
-}) {
-  return <p>Hello {hello}, I am a server component with children: {children}</p>
+type TypeHack<T> = [never, T]
+type RemoteForwardedRef<T> = "remote-component-element" | TypeHack<T>
+type RemoteRefAttributes<T> = {} | TypeHack<T>
+
+function remoteForwardRef<TProps, TRef, TReturn extends (JSX.Element | Promise<JSX.Element>)>(
+  componentFn: (props: TProps, ref: RemoteForwardedRef<TRef>) => TReturn
+): (props: TProps & RemoteRefAttributes<TRef>) => TReturn {
+  return (props) => componentFn(props, "remote-component-element")
 }
+
+export const MyServerComponent = remoteForwardRef(async (
+  { hello, children }: { hello: string, children: React.ReactNode },
+  ref: RemoteForwardedRef<HTMLParagraphElement>,
+) => {
+  // HACK Server-side React ignores (i.e. does not render) `ref` attribute, so use `data-ref`
+  return <p data-ref={ref}>Hello {hello}, I am a server component with children: {children}</p>
+})
