e8025c7121da690a3c842505e95c9c1744a903e7
Jonathan Hefner <jonathan@hefner.pro>
2024-11-22 13:24:24 -0600

    Factor out implementation core

---
+++ lib/rrc-client.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useEffect, useState } from "react"
+import { createPortal } from "react-dom"
+import type { RemoteComponentSet } from "./rrc-server"
+
+async function fetchRemoteComponentHtml(name: string, route: string, propsJson: string): Promise<string> {
+  // TODO Use HTTP `QUERY` method when supported
+  // See https://datatracker.ietf.org/doc/draft-ietf-httpbis-safe-method-w-body/
+  const response = await fetch(`${route}/${name}?${new URLSearchParams({ p: propsJson })}`)
+  if (!response.ok) throw new Error(`Could not fetch ${name} component (${response.status})`)
+  return await response.text()
+}
+
+function setRef(ref: React.ForwardedRef<HTMLElement | null>, el: HTMLElement | null) {
+  if (typeof ref === "function") {
+    ref(el)
+  } else if (ref) {
+    ref.current = el
+  }
+}
+
+type Props = { children?: React.ReactNode, ref?: React.ForwardedRef<HTMLElement | null> }
+
+function renderRemoteComponent(name: string, route: string, props: Props) {
+  let children, ref
+  ({ children, ref, ...props } = props)
+  const propsJson = JSON.stringify(props)
+
+  const [html, setHtml] = useState("")
+
+  useEffect(() => {
+    fetchRemoteComponentHtml(name, route, propsJson).then(setHtml)
+  }, [propsJson])
+
+  const [childrenSlots, setChildrenSlots] = useState<HTMLElement[]>([])
+
+  const renderHtml = useCallback((el: HTMLElement) => {
+    // TODO Use `dangerouslySetInnerHTML` when https://github.com/facebook/react/issues/31600 is fixed?
+    if (el) el.innerHTML = html
+
+    // TODO Avoid calling when `el` is not null but `[data-ref=remote-component-element]` does not exist?
+    if (ref) setRef(ref, el?.querySelector("[data-ref=remote-component-element]"))
+
+    setChildrenSlots(Array.from(el?.querySelectorAll("[data-children-slot]") ?? []))
+  }, [html, ref])
+
+  return <>
+    <slot ref={renderHtml as React.Ref<HTMLSlotElement>} style={{ display: "contents" }} />
+    {childrenSlots.map(slot => createPortal(children, slot))}
+  </>
+}
+
+type ResolvedComponentSet<TSet extends RemoteComponentSet> = {
+  [key in keyof TSet]: (props: React.ComponentProps<TSet[key]>) => React.JSX.Element
+}
+
+export function useRemoteComponents<T extends RemoteComponentSet>(route: string): ResolvedComponentSet<T> {
+  return new Proxy({}, {
+    get(memo: any, name: string) {
+      memo[name] ??= (props: Props) => renderRemoteComponent(name, route, props)
+      return memo[name]
+    }
+  })
+}
---
+++ lib/rrc-server.tsx
@@ -0,0 +1,51 @@
+import { NextRequest } from "next/server"
+import { PassThrough } from "stream"
+
+// TODO Switch to `renderToReadableStream` when https://github.com/facebook/react/issues/26906 is fixed
+//
+// HACK Workaround for error:
+//    You're importing a component that imports react-dom/server. To fix it, render or return the content directly as a Server Component instead for perf and security.
+// See also https://github.com/vercel/next.js/issues/71865#issuecomment-2444377726
+import type { renderToPipeableStream as Type_renderToPipeableStream } from "react-dom/server"
+let renderToPipeableStream: typeof Type_renderToPipeableStream
+import("react-dom/server").then(module => {
+  renderToPipeableStream = module.renderToPipeableStream
+})
+
+
+type RemoteJSXElementConstructor<P> =
+  | ((props: P) => React.JSX.Element)
+  | ((props: P) => Promise<React.JSX.Element>)
+
+export type RemoteComponentSet = { [key: string]: RemoteJSXElementConstructor<any> }
+
+type RemoteComponentRouteHandler = (
+  request: NextRequest,
+  arg: { params: Promise<{ component: string }> }
+) => Promise<Response>
+
+function ChildrenSlot() {
+  return <slot data-children-slot style={{ display: "contents" }} />
+}
+
+export function serveRemoteComponents(components: RemoteComponentSet): RemoteComponentRouteHandler {
+  return async (request, { params }) => {
+    const Component = components[(await params).component]
+    if (!Component) return new Response(null, { status: 404 })
+
+    const propsJson = request.nextUrl.searchParams.get("p")
+    const props = propsJson ? JSON.parse(propsJson) : {}
+
+    const stream = new PassThrough()
+    renderToPipeableStream(<Component {...props} children={<ChildrenSlot />} />).pipe(stream)
+    return new Response(stream as any)
+  }
+}
+
+export type RemoteForwardedRef<R> = "remote-component-element" | [never, R]
+
+export function remoteForwardRef<TProps, TRef, TReturn extends (React.JSX.Element | Promise<React.JSX.Element>)>(
+  componentFn: (props: TProps, ref: RemoteForwardedRef<TRef>) => TReturn
+): (props: TProps & React.RefAttributes<TRef>) => TReturn {
+  return (props) => componentFn(props, "remote-component-element")
+}
