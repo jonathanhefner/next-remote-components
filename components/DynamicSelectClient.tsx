c2a5ec0ba4650fb97c56252053a5a6d91fce7fc7
Jonathan Hefner <jonathan@hefner.pro>
2024-11-23 20:22:07 -0600

    Add dynamic select example

---
+++ components/DynamicSelectClient.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useRemoteComponents } from "@/lib/rrc-client"
+import { Suspense, useState } from "react"
+import type myRemoteComponents from "./my-remote-components"
+
+const { DynamicSelectServer } = useRemoteComponents<typeof myRemoteComponents>("/api/remote-components")
+
+export default function DynamicSelectClient() {
+  const [batch, setBatch] = useState("batch1")
+
+  return <>
+    <select value={batch} onChange={e => setBatch(e.target.value)}>
+      <option value="batch1">Batch 1</option>
+      <option value="batch2">Batch 2</option>
+      <option value="batch3">Batch 3</option>
+    </select>
+    <DynamicSelectServer batch={batch} />
+  </>
+}
