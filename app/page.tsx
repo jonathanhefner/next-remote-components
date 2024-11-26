4e5c8ab4477658e08557bb9e269608006187de52
Jonathan Hefner <jonathan@hefner.pro>
2024-11-26 13:26:28 -0600

    Add typeahead example

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import DynamicSelectClient from "@/components/DynamicSelectClient"
 import MyClientComponent from "@/components/MyClientComponent"
+import Typeahead from "@/components/Typeahead"
 
 export default function Home() {
   return <>
     <MyClientComponent />
     <hr />
     <DynamicSelectClient />
+    <hr />
+    <Typeahead />
   </>
 }
---
+++ components/TypeaheadSuggestions.tsx
@@ -0,0 +1,11 @@
+export default async function TypeaheadSuggestions({ id, inputValue }: { id: string, inputValue: string }) {
+  const suggestions = [...Array(20).keys().map(i => `${inputValue}${i}`)]
+
+  return (
+    <datalist id={id}>
+      {suggestions.map(value =>
+        <option key={value} value={value} />
+      )}
+    </datalist>
+  )
+}
