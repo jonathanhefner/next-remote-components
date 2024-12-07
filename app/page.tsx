14490811bc36307e95d27df2563fd41086a7f488
Jonathan Hefner <jonathan@hefner.pro>
2024-12-07 13:44:19 -0600

    Cross-link app and repo

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,29 @@ export default function Home() {
   return <>
     <section className="basic-example">
       <h2>Basic Example</h2>
-      <div><MyClientComponent /></div>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/next-remote-components/blob/main/components/MyServerComponent.tsx">Server Component</a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/next-remote-components/blob/main/components/MyClientComponent.tsx">Client Component</a>
+      </p>
+      <div className="demo"><MyClientComponent /></div>
     </section>
 
     <section className="dynamic-select">
       <h2>Dynamic Select + Suspense</h2>
-      <div><DynamicSelectClient /></div>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/next-remote-components/blob/main/components/DynamicSelectServer.tsx">Server Component</a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/next-remote-components/blob/main/components/DynamicSelectClient.tsx">Client Component</a>
+      </p>
+      <div className="demo"><DynamicSelectClient /></div>
     </section>
 
     <section className="typeahead">
       <h2>Typeahead</h2>
-      <div><Typeahead /></div>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/next-remote-components/blob/main/components/TypeaheadSuggestions.tsx">Server Component</a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/next-remote-components/blob/main/components/Typeahead.tsx">Client Component</a>
+      </p>
+      <div className="demo"><Typeahead /></div>
     </section>
   </>
 }
