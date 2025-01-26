51cff5bb8ce8086e8419833b6b4df65d291c6f1e
Jonathan Hefner <jonathan@hefner.pro>
2025-01-25 20:57:52 -0600

    Rework to use custom suspense boundary

--- components/DynamicSelect.tsx
+++ components/DynamicSelect.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { useRemote } from "@/lib/rrc"
+import { remote, RemoteSuspense } from "@/lib/rrc"
 import { useState } from "react"
 import { getItemOptions } from "./DynamicSelect.server"
 
+const ItemOptions = remote(getItemOptions)
+
 export default function DynamicSelect() {
-  const [ItemOptions, isLoading] = useRemote(getItemOptions)
   const [batch, setBatch] = useState("batch1")
 
   return <>
@@ -15,8 +16,18 @@ export default function DynamicSelect() {
       <option value="batch3">Batch 3</option>
     </select>
 
-    <select disabled={isLoading}>
-      <ItemOptions batch={batch} fallback={<option>Loading...</option>} />
-    </select>
+    <RemoteSuspense fallback={<Loading />}>
+      <select>
+        <ItemOptions batch={batch} />
+      </select>
+    </RemoteSuspense>
   </>
+}
+
+function Loading() {
+  return (
+    <select disabled>
+      <option>Loading...</option>
+    </select>
+  )
 }
--- lib/rrc.tsx
+++ lib/rrc.tsx
@@ -1,53 +1,75 @@
-import { startTransition, useCallback, useContext, useEffect, useId, useState } from "react"
-import { createContext } from "react"
+import { createContext, memo, Suspense, use, useContext, useEffect, useId, useRef, useState } from "react";
 
 export type RemoteComponent<TProps> = (props: TProps) => Promise<React.ReactNode>
 
-export type UsingRemoteComponent<TProps> = [
-  component: React.FunctionComponent<TProps & { fallback?: React.ReactNode }>,
-  isLoading: boolean,
-]
-
-const INITIAL_PROPS = { uniqueValue: Symbol() }
-
-export function useRemote<TProps = {}>(
-  remoteComponent: RemoteComponent<TProps>
-): UsingRemoteComponent<TProps> {
-  const [isLoading, setIsLoading] = useState(false)
-
-  const component = useCallback((rawProps: {}) => {
-    let { children, fallback, ...props } = rawProps as any
-    const [renderingProps, setRenderingProps] = useState<{}>(INITIAL_PROPS)
-    const [renderedProps, setRenderedProps] = useState<{}>({})
-    const [rendered, setRendered] = useState<React.ReactNode>(undefined)
-    const childrenKey = `${useId()}-children`
-
-    if (isEquivalent(props, renderingProps)) {
-      props = renderingProps
-    }
-
-    useEffect(() => {
-      setIsLoading(true)
-      setRenderingProps(props)
-      startTransition(async () => {
-        const remoteProps = children ? { ...props, children: <ChildrenPortal key={childrenKey} /> } : props
-        setRendered(await remoteComponent(remoteProps as TProps))
-        setRenderedProps(props)
-        setIsLoading(false)
-      })
-    }, [props])
-
-    return (
-      <ChildrenPortalContext.Provider value={children}>
-        {isEquivalent(renderingProps, renderedProps) ? rendered : fallback}
-      </ChildrenPortalContext.Provider>
-    )
-  }, [setIsLoading])
-
-  return [component, isLoading]
+type PromiseCacheEntry = {
+  promise: Promise<React.ReactNode>,
+  props: object,
 }
 
-function isEquivalent(object1: {}, object2: {}) {
+type RemoteContextValue = {
+  promiseCache: PromiseCacheEntry[],
+  promiseCoordinator: PromiseWithResolvers<void>,
+}
+
+const RemoteContext = createContext<RemoteContextValue | null>(null)
+
+const ChildrenSlotContext = createContext<React.ReactNode | undefined>(undefined)
+
+export function remote<TProps extends object>(
+  remoteComponent: RemoteComponent<TProps>
+): React.FunctionComponent<TProps> {
+  const MemoizedComponent = memo((props: TProps) => use(getPromise(remoteComponent, props)))
+
+  return (props: React.PropsWithChildren<TProps>) => {
+    const children = props.children
+    if ("children" in props) {
+      props = { ...props, children: true }
+    }
+
+    return (
+      <ChildrenSlotContext.Provider value={children}>
+        <MemoizedComponent {...props} />
+      </ChildrenSlotContext.Provider>
+    )
+  }
+}
+
+function ChildrenSlot() {
+  const children = useContext(ChildrenSlotContext)
+  return children
+}
+
+function getPromise<TProps extends object>(
+  remoteComponent: RemoteComponent<TProps>,
+  props: TProps,
+): Promise<React.ReactNode> {
+  const context = useContext(RemoteContext)
+  if (!context) throw new Error("Remote component must be wrapped in RemoteSuspense boundary")
+
+  const id = useId()
+
+  let cacheEntry = context.promiseCache.find(cacheEntry => isEquivalent(props, cacheEntry.props))
+
+  if (!cacheEntry) {
+    const remoteProps = "children" in props
+      ? { ...props, children: <ChildrenSlot key={`${id}-children`} /> }
+      : props
+
+    cacheEntry = {
+      // TODO After https://github.com/vercel/next.js/issues/75374 is fixed,
+      // remove `promiseCoordinator` and call server function directly.
+      promise: context.promiseCoordinator.promise.then(() => remoteComponent(remoteProps)),
+      props: props,
+    }
+
+    context.promiseCache.push(cacheEntry)
+  }
+
+  return cacheEntry.promise
+}
+
+function isEquivalent(object1: object, object2: object) {
   let parity = 0
 
   for (const key in object1) {
@@ -66,9 +88,45 @@ function isEquivalent(object1: {}, object2: {}) {
   return parity === 0
 }
 
-const ChildrenPortalContext = createContext<React.ReactNode>(null)
+export function RemoteSuspense({ fallback, children }: {
+  fallback?: React.ReactNode,
+  children?: React.ReactNode | undefined,
+}) {
+  const contextStateRef = useRef<RemoteContextValue>({
+    promiseCache: [],
+    promiseCoordinator: Promise.withResolvers(),
+  })
 
-function ChildrenPortal() {
-  const children = useContext(ChildrenPortalContext)
-  return children
+  // Avoid server function calls during SSR and hydration errors during CSR
+  //
+  // TODO After https://github.com/vercel/next.js/issues/53987 is fixed, simply
+  // throw Error in `getPromise` when `window` is undefined.
+  const [isFirstRender, setIsFirstRender] = useState(true)
+  useEffect(() => { setIsFirstRender(false) }, [])
+  if (isFirstRender) return fallback
+
+  return (
+    <RemoteContext.Provider value={contextStateRef.current}>
+      <Suspense fallback={<RemoteManager fallback={fallback} />}>
+        {children}
+      </Suspense>
+    </RemoteContext.Provider>
+  )
+}
+
+function RemoteManager({ fallback }: {
+  fallback: React.ReactNode
+}) {
+  const context = useContext(RemoteContext)!
+
+  useEffect(() => {
+    context.promiseCoordinator.resolve()
+
+    return () => {
+      context.promiseCache = []
+      context.promiseCoordinator = Promise.withResolvers()
+    }
+  }, [context])
+
+  return fallback
 }
